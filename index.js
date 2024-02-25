cc35a6e9f1a2b71ff40ffddf1af390285504b84e
The-C2020 <75270127+The-C2020@users.noreply.github.com>
2024-02-25 21:21:41 +0100

    Add files via upload
    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+new Twitch.Embed("twitch-embed", {
+	width: "100%",
+	layout: "video",
+	muted: true,
+	channel: "puschkin_bmb",
+	// Only needed if this page is going to be embedded on other websites
+	//parent: ["embed.example.com", "othersite.example.com"],
+});
