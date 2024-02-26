fd54ad7b757f36f31d0c2a1ceb856ed176997d7c
The-C2020 <75270127+The-C2020@users.noreply.github.com>
2024-02-26 17:08:18 +0100

    Add files via upload
    Second try to fix jumping of background image

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 new Twitch.Embed("twitch-embed", {
-	width: "100%",
-	layout: "video",
-	muted: true,
-	channel: "puschkin_bmb",
-	// Only needed if this page is going to be embedded on other websites
-	//parent: ["embed.example.com", "othersite.example.com"],
+  width: "100%",
+  layout: "video",
+  muted: true,
+  channel: "puschkin_bmb",
+  // Only needed if this page is going to be embedded on other websites
+  //parent: ["embed.example.com", "othersite.example.com"],
+});
+
+window.addEventListener("resize", function () {
+  document.body.style.backgroundSize = window.innerWidth + "px " + window.innerHeight + "px";
 });
